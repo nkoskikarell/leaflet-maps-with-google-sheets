0e89a56e4357f561a10f2099e62574ef4a07156c
nkoskikarell <57502886+nkoskikarell@users.noreply.github.com>
2020-02-19 13:00:42 -0800

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var https://docs.google.com/spreadsheets/d/1YKWmQvq7zo6VS2a4XkbqM-OkmuKnxjsFJZgyrecOr5s/edit#gid=0
